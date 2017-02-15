91660f8ae7c013fd49bbfe2fa7d44d228b31ffc6
K.P. Sullivan <MacabreSkull@gmail.com>
2017-02-15 04:48:47 -0700

    Added stuff

---
+++ If_Else/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+    public string Animal;
+    // Use this for initialization
+    void Start()
+    {
+        switch (Animal)
+        {
+            case ("Dog"):
+                print("Dog");
+                break;
+            case ("Cat"):
+                print("Cat");
+                    break;
+            case ("Frog"):
+                print("Frogs are the superior animal");
+                break;
+
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
