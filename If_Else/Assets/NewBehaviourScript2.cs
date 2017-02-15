27f7ad3de7bb1a5f91848d8c41de63faa87fa6d2
K.P. Sullivan <MacabreSkull@gmail.com>
2017-02-15 10:52:08 -0700

    Just the script

---
+++ If_Else/Assets/NewBehaviourScript2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript2 : MonoBehaviour {
+
+    int i = 0;
+    public string[] colors;
+
+	// Use this for initialization
+	void Start () {
+        while(i < colors.Length)
+        {
+            print(colors[i]);
+            (i)++;
+        }
+        print("done");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
