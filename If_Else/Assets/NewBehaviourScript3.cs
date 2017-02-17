4315135ad6e8e7ada7595d1258a5c409c51af841
K.P. Sullivan <MacabreSkull@gmail.com>
2017-02-17 10:18:24 -0700

    Another scene?
    Why did it make another scene, I saved as another scene to make sure nothing is damaged

---
+++ If_Else/Assets/NewBehaviourScript3.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame, with KeyCode try to isolate them all into ONE update.
+	void Update() {
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("A");
+        }
+        else
+        {
+            print("No input");
+        }
+    }
+}
