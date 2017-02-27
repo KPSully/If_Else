e1f23d01d98bf73cf5e3ad32cd019607388f823d
K.P. Sullivan <MacabreSkull@gmail.com>
2017-02-27 04:26:25 -0700

    Can't seem to get it to work

---
+++ If_Else/Assets/Add.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Add : MonoBehaviour
+{
+    public string theButton;
+    public listInit.strings listString;
+
+    public void AddToList()
+    {
+        listString.stringsList.Add(theButton);
+    }
+//    public void Add{
+//        listStrings add theButton
+//    }
+}
---
+++ If_Else/Assets/listInit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class listInit : MonoBehaviour
+{
+    public class strings
+    {
+        public List<string> stringsList;
+
+        //       public List<string> stringsList;
+
+        void Start()
+        {
+            for (int i = 0; i < stringsList.Count; i++)
+            {
+                print(stringsList[i]);
+            }
+        }
+    }
+}
