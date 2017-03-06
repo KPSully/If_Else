259976a691f87af47a410960e0d9e5999315dd3a
K.P. Sullivan <MacabreSkull@gmail.com>
2017-03-06 10:52:59 -0700

    Scope

---
+++ If_Else/Assets/scope.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scope : MonoBehaviour {
+
+    public string userName = "Bob";
+
+	// Use this for initialization
+    void Awake ()
+    {
+        string myName = "Joe";
+        if(myName == "Joe")
+        {
+            string otherName = "Helen";
+            print(myName + "ate" + otherName);
+        }
+        print(myName);
+        print(userName);
+    }
+	void Start () {
+        string myName = "Jeff";
+        print(userName);
+        print(myName);
+	}
+}
