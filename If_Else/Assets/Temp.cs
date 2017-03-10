4b15a5d5978d1a45bf6c0fe77a7fde454cb9e7f8
K.P. Sullivan <MacabreSkull@gmail.com>
2017-03-10 10:38:44 -0700

    Temp

---
+++ If_Else/Assets/Temp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Temp : MonoBehaviour {
+    public float temp = 50.0f;
+	public void FtoC () {
+        float ctemp;
+
+        ctemp = (temp - 32.0f) * (5.0f / 9.0f);
+	}
+	public void CtoF () {
+        float ftemp;
+        ftemp = (temp * (4.0f / 9.0f) + 32.0f);
+	}
+}
